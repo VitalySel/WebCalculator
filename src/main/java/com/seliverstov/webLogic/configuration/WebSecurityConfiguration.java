c718641fa3e48fb2b7423865bf8e0d60b2a2f49c
Seliverstov Vitaly <seliverstov_vitaly@mail.ru>
2019-12-13 20:34:15 +0400

    Deploy to heroku

--- src/main/java/com/seliverstov/webLogic/configuration/WebSecurityConfiguration.java
+++ src/main/java/com/seliverstov/webLogic/configuration/WebSecurityConfiguration.java
@@ -16,7 +16,6 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 @EnableWebSecurity
-@EnableGlobalMethodSecurity(prePostEnabled = true)
 public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
 
     @Autowired
@@ -31,7 +30,7 @@ public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http
                 .authorizeRequests()
-                .antMatchers("/greeting","/registration").permitAll()
+                .antMatchers("/","/registration").permitAll()
                 .anyRequest().authenticated()
                 .and()
                 .formLogin()
